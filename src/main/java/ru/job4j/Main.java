0c13584bcb232a66350c558d0d25ed561ad4f164
kmc <do.88000@gmail.com>
2026-09-16 14:23:44 +0300

    #505269 #528911 -- Настройка окружения, загрузка файлов.

---
+++ src/main/java/ru/job4j/Main.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        System.out.println("Привет Мир!");
+        // git@github.com:mskorolyow/job4j_elementary.git
+    }
+}
